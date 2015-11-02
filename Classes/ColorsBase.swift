88df0c4f12dbfbfa955cd05ce9e888528f03b1c9
Airat Uraev <airat.uraev@gmail.com>
2015-11-02 14:34:50 +0300

    update color base

--- Classes/ColorsBase.swift
+++ Classes/ColorsBase.swift
@@ -16,7 +16,7 @@ public class ColorsBase {
         var formatter = NSNumberFormatter()
         return formatter}()
     
-    class func colorFromText(hexText: String) -> UIColor! {
+    public class func colorFromText(hexText: String) -> UIColor! {
         let hexValue: Int! = numberFormatter.numberFromString(hexText.stringByReplacingOccurrencesOfString("#", withString: ""))?.integerValue
         var color: UIColor!
         
@@ -27,7 +27,7 @@ public class ColorsBase {
         return color
     }
     
-    class func colorFromHex(hexValue: UInt) -> UIColor {
+    public class func colorFromHex(hexValue: UInt) -> UIColor {
         var alpha: CGFloat! = 1.0
         
         if hexValue > 0xFFFFFF {
@@ -37,7 +37,7 @@ public class ColorsBase {
         return UIColor(red: CGFloat(((hexValue & 0xFF0000) >> 16))/255.0, green: CGFloat(((hexValue & 0xFF00) >> 8))/255.0, blue: CGFloat(((hexValue & 0xFF) >> 0))/255.0, alpha: alpha)
     }
     
-    class func textFromColor(color: UIColor) -> String {
+    public class func textFromColor(color: UIColor) -> String {
         var result: String
         let ciColor: CIColor = CIColor(CGColor: color.CGColor)
         
